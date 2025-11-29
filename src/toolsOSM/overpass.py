0253a2f594e174d6c4965e0e6ddb6f37845de219
diego vega <da-vce@hotmail.com>
2025-11-29 18:36:51 -0500

    Add new functionality for OSM basic tests and enhance logging capabilities

--- src/toolsOSM/overpass.py
+++ src/toolsOSM/overpass.py
@@ -6,9 +6,11 @@ import pandas as pd
 import time
 import logging
 import toolsGeneral.main as tgm
+import toolsGeneral.logger as tgl
 from pathlib import Path
+import re
 
-logger = logging.getLogger('dup_test_logger')
+logger = logging.getLogger('logger')
 raw_scrape_logger = logging.getLogger('raw_scrape_logger')
 
 def getOSMIDAddsStruct(relId: str, lvls: list):
@@ -142,32 +144,6 @@ def getOSMIDAddsStruct_chunks(tuple, save_dir:Path):
     fetch_level_with_retry('6', '8', state)
 
     return state
-
-def fetch_admin_osm_structure(tuple, save_dir:Path, method='simple'):
-    failed_countries_path = save_dir / 'failed_countries.pkl'
-    failed_countries = tgm.load(failed_countries_path) if os.path.exists(failed_countries_path) else {}
-
-    country, id, addLvls = tuple
-    raw_scrape_logger.info(f"* processing: {country, id, addLvls}")
-
-    match method:
-        case 'simple':
-            response = getOSMIDAddsStruct(id, addLvls)
-            raw_scrape_logger.info(f"  - finished: {response['status']}")
-            save_path = save_dir / country / f'rawOSMRes.json'
-            if response["status"] == "ok":
-                tgm.dump(save_path, response["data"])
-            elif '429 Client Error' in response["status_type"]:
-                raw_scrape_logger.info(f"  - Too many requests error, trying chunks")
-                response = getOSMIDAddsStruct_chunks(tuple, save_dir)
-            else:
-                failed_countries[country] = {"id": tuple, "response": response}
-                tgm.dump(failed_countries_path, failed_countries)
-        case 'chunks':
-            response = getOSMIDAddsStruct_chunks(tuple, save_dir)
-
-    time.sleep(3)
-    return response
 
 def get_add_lvls_from_id(ids:list, lvl:str):
 
@@ -565,3 +541,136 @@ def osm_query_safe_wrapper(query, max_retries=5):
         time.sleep(min(2**attempt, 15))
 
     return {"status": "error", "status_type": status_type, "data": None}
+
+
+def osm_basic_test(df_input, save_logger_path = None):
+
+    if save_logger_path:
+        tgl.add_file_handler(logger, save_logger_path)
+
+    #* sort the dataframe
+    df = df_input.copy()
+    df = df.sort_values('tags.admin_level', key=lambda col: col.astype(int)) 
+
+    cntrRow = df["tags.ISO3166-1"].notna()
+    cntrISO = df[cntrRow].iloc[0]["tags.ISO3166-1"]
+    cntrName = df[cntrRow].iloc[0]["tags.country_name"]
+    cntr_id = df[cntrRow].iloc[0]["id"]
+
+    #* some elements have missing name
+    miss = df[df["tags.name"].isna()]['id'].to_list()
+    logger.info(f" * missing names: {len(miss)}")
+
+    #* relations from other countries
+    #* only discard the ones we are sure are not from the country
+    #* pass the test otherwise
+
+    checkTags = [
+        "tags.is_in:country",
+        "tags.ISO3166-2",
+        "tags.ref:nuts",
+        "tags.ref:nuts:2",
+        "tags.ref:nuts:3",
+        "tags.addr:country"
+    ]
+    leak = []
+    in_country = []
+    isInCountry = {}
+    NA_result = []
+
+    for idx, row in df.iterrows():
+        true_count = 0
+        false_count = 0
+        osmID = str(row.get("id"))
+        if str(row.get("tags.admin_level")) == '2':
+            isInCountry[osmID] = True
+            in_country.append((osmID, pd.NA, cntr_id))
+            continue
+        parentID = row.get("tags.parent_id")
+
+        for tag in checkTags:
+            # we'll check all tags to have some confidence meassure of the test
+            val = row.get(tag)
+            if pd.isna(val):
+                continue
+
+            # Handle is_in:country tag
+            if tag == "tags.is_in:country":
+                if val.strip().lower() == cntrName.strip().lower():
+                    true_count += 1
+                else:
+                    false_count += 1
+                continue
+
+            # Handle ISO-style tags
+            checkISO_res = checkISO(val, cntrISO)
+            if checkISO_res is True:
+                true_count += 1
+            elif checkISO_res is False:
+                false_count += 1
+            # else NA, ignore
+
+
+        # meassure confidence of results
+
+        if true_count >= 2:
+            in_country.append((osmID, parentID, cntr_id))
+            isInCountry[osmID] = True
+        elif false_count > 0:
+            leak.append((osmID, parentID, cntr_id))
+            isInCountry[osmID] = False
+        elif true_count <= 1:
+            # single weak signal, fallback to parent
+            # except for first level (4)
+            if parentID and isInCountry.get(parentID) is True and row.get('tags.admin_level') != '4':
+                in_country.append((osmID, parentID, cntr_id))
+                isInCountry[osmID] = True
+            else:
+                NA_result.append((osmID, parentID, cntr_id))
+                isInCountry[osmID] = pd.NA
+
+    logger.info(f" * relations from other countries: {len(leak)}")
+
+    return {    
+        "missing.name": miss,
+        "leak": leak,
+        "in_country": in_country,
+        'NA_result': NA_result,
+    }
+
+def checkISO(code, cntrCode):
+    if code == "": return pd.NA
+    iso1 = re.search(r"^([A-Z]{2})", code)
+
+    if not iso1:
+        return pd.NA
+    else:
+        iso1 = iso1.group(0)
+
+    return iso1 == cntrCode
+
+def osm_test_center(rows, save_temp=False, save_path=''):
+
+    total = len(rows)
+    if save_temp:
+        test_res = tgm.load(save_path) if os.path.exists(save_path) else {}
+
+    for i, (idx, row) in enumerate(rows.iterrows(), start=1):
+        logger.info(f'* country: {Path(save_path).parent.name}')
+        tuple_id = (row["id"], row["tags.parent_id"], row["tags.country_id"])
+        logger.info(f" ^ [{i}/{total}]: testing {tuple_id}:")
+
+        res = is_child_inside_parent(row["id"], row["tags.parent_id"])
+        test_res[tuple_id] = res
+
+        resume  = {k:v['status'] for k,v in res.items()}
+        status_list = [v['status'] for k,v in res.items()]
+
+        if save_temp and 'error' not in status_list:
+            logger.info(f"  * saving ...")
+            tgm.dump(save_path, test_res)
+
+        logger.info(f" $ finished: status: {resume}")
+        
+        time.sleep(3)
+    return test_res
