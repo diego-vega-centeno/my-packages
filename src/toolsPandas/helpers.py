4830b532fc33cdd74c7e46ca1f293cb439c1e8a2
diego vega <da-vce@hotmail.com>
2025-12-02 13:25:46 -0500

    Add toml dependencies Register df_peek with pandas_flavor

--- src/toolsPandas/helpers.py
+++ src/toolsPandas/helpers.py
@@ -1,6 +1,7 @@
 from flatten_json import unflatten
 from IPython.display import display, HTML
 import pandas as pd
+import pandas_flavor as pf
 
 def peek(df):
     return df.dropna(axis=1, how="all").map(repr)
@@ -12,6 +13,7 @@ def unflatten(df):
         for _, row in df.iterrows()
     ]
 
+@pf.register_dataframe_method
 def df_peek(df,  height=500, width='100%'):
     html = f"""
     <div style="overflow:auto; max-height:{height}px; max-width:{width}; border:1px solid #ccc; padding:4px;">
@@ -21,7 +23,7 @@ def df_peek(df,  height=500, width='100%'):
     display(HTML(html))
 
 def to_df(dict):
-    return pd.DataFrame(dict)
+    return pd.DataFrame(dict).T
 
 def get_from_df(df, columns, vals):
     return df[(df[columns] == vals).all(axis=1)]
