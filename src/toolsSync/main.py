a1c43b1f79920fa8759c7edbd0c3a0b0e36dbb08
diego vega <da-vce@hotmail.com>
2025-12-09 12:20:04 -0500

    Define donwload_country_data_from_bucket

--- src/toolsSync/main.py
+++ src/toolsSync/main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import os
 import datetime
 from botocore.exceptions import ClientError
+import re
 
 def upload_dir_files_to_backblaze(dir:Path, config):
 
@@ -51,3 +52,32 @@ def update_process_state(process_state, country, process_type, process_status="p
     process_state[country][process_type]['status'] = process_status
     process_state[country][process_type]['last_run'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     process_state[country][process_type]['error'] = process_error
+
+def donwload_country_data_from_bucket(countries, bucket_dir:Path, save_dir:Path, s3, logger):
+    downloaded_count = 0
+    to_download_total = 0
+    list_obj_response = s3.list_objects_v2(Bucket=os.environ["B2_BUCKET_NAME"], Prefix=bucket_dir.as_posix())
+    files_list = [(obj['Key']) for obj in list_obj_response['Contents']]
+    logger.info(f"Total files found for bucket in '{bucket_dir}': {len(files_list)}")
+
+    logger.info(f"* Total of countries to download: {len(countries)}")
+    # load data from b2 bucket for countries to process
+    for count, country in enumerate(countries):
+        country_files = [str(file) for file in files_list if re.match(rf"{bucket_dir.as_posix()}/{country}/.+\.json", file)]
+        to_download_total += len(country_files)
+        logger.info(f"* Country {country} ({count}/{len(countries)}) files found: {len(country_files)}")
+        for file in country_files:
+            save_file = save_dir / country / os.path.basename(file)
+            if save_file.exists():
+                logger.info(f"  * Skip existing file {save_file}")
+                continue
+            
+            os.makedirs(save_file.parent, exist_ok=True)
+            try:
+                s3.download_file(os.environ["B2_BUCKET_NAME"], file, str(save_file))
+                logger.info(f"  * File '{file}' downloaded successfully to '{save_file}'")
+                downloaded_count += 1
+            except Exception as e:
+                logger.error(f"  * Error downloading file '{file}': {e}")
+
+    logger.info(f"* Number of downloaded files: {downloaded_count}/{to_download_total}")
