36a9378c17c5d90d681df492d510298b4b20b3bb
diego vega <da-vce@hotmail.com>
2025-12-09 19:22:17 -0500

    Refactor donwload_country_data_from_bucket include check with B2 objects

--- src/toolsSync/main.py
+++ src/toolsSync/main.py
@@ -53,31 +53,49 @@ def update_process_state(process_state, country, process_type, process_status="p
     process_state[country][process_type]['last_run'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     process_state[country][process_type]['error'] = process_error
 
-def donwload_country_data_from_bucket(countries, bucket_dir:Path, save_dir:Path, s3, logger):
-    downloaded_count = 0
-    to_download_total = 0
-    list_obj_response = s3.list_objects_v2(Bucket=os.environ["B2_BUCKET_NAME"], Prefix=bucket_dir.as_posix())
-    files_list = [(obj['Key']) for obj in list_obj_response['Contents']]
-    logger.info(f"Total files found for bucket in '{bucket_dir}': {len(files_list)}")
+def donwload_country_data_from_bucket(countries, bucket_name, bucket_dir:Path, save_dir:Path, s3, logger):
 
-    logger.info(f"* Total of countries to download: {len(countries)}")
+    list_obj_response = s3.list_objects_v2(Bucket=bucket_name, Prefix=bucket_dir.as_posix())
+
+    objects_list = [(obj['Key']) for obj in list_obj_response['Contents']]
+    b2_countries = tgm.deleteDuplicates([Path(obj['Key']).parent.name  for obj in list_obj_response['Contents']])
+
+    logger.info(f"  * Total objects found in B2 '{bucket_dir}': {len(objects_list)}")
+    logger.info(f"  * Total countries found in B2 '{bucket_dir}': {len(b2_countries)}")
+
+    logger.info(f"  * Countries to download: {len(countries)}: {countries}")
+    countries_in_b2 = tgm.intersection(countries, b2_countries)
+    logger.info(f"  * Countries to download found in B2: {len(countries_in_b2)}")
+    logger.info(f"  * Countries to download missing in B2: {len(tgm.complement(countries_in_b2, b2_countries))}")
+
+    logger.info(f"  * Downloading data for countries: {len(countries_in_b2)}")
+    logger.info(f"  * Downloading directory: '{bucket_dir}' -> '{save_dir}'")
+
+    downloaded_countries = []
+    downloaded_files_count = 0
+    to_download_total = 0
     # load data from b2 bucket for countries to process
-    for count, country in enumerate(countries):
-        country_files = [str(file) for file in files_list if re.match(rf"{bucket_dir.as_posix()}/{country}/.+\.json", file)]
+    for count, country in enumerate(countries ,start=1):
+        country_files = [str(file) for file in objects_list if re.match(rf"{bucket_dir.as_posix()}/{country}/.+\.json", file)]
         to_download_total += len(country_files)
-        logger.info(f"* Country {country} ({count}/{len(countries)}) files found: {len(country_files)}")
+        logger.info(f"    * ({count}/{len(countries)}) Country {country} files found: {len(country_files)}")
+        if len(country_files) < 1:
+            continue
         for file in country_files:
             save_file = save_dir / country / os.path.basename(file)
             if save_file.exists():
-                logger.info(f"  * Skip existing file {save_file}")
+                logger.info(f"      * Skip existing file {save_file.name}")
+                downloaded_files_count += 1
                 continue
             
             os.makedirs(save_file.parent, exist_ok=True)
             try:
                 s3.download_file(os.environ["B2_BUCKET_NAME"], file, str(save_file))
-                logger.info(f"  * File '{file}' downloaded successfully to '{save_file}'")
-                downloaded_count += 1
+                logger.info(f"      * File '{os.path.basename(file)}' downloaded successfully to '{save_file.name}'")
+                downloaded_files_count += 1
             except Exception as e:
-                logger.error(f"  * Error downloading file '{file}': {e}")
+                logger.error(f"      * Error downloading file '{os.path.basename(file)}': {e}")
+        downloaded_countries.append(country)
 
-    logger.info(f"* Number of downloaded files: {downloaded_count}/{to_download_total}")
+    logger.info(f"  * Number of downloaded files: {downloaded_files_count}/{to_download_total}")
+    return downloaded_countries
