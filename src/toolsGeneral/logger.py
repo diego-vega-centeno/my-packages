9cfbbef68c4b6c1423253114df2fb046742d0448
diego vega <da-vce@hotmail.com>
2025-12-23 23:50:23 -0500

    Add date to formatter

--- src/toolsGeneral/logger.py
+++ src/toolsGeneral/logger.py
@@ -8,21 +8,26 @@ def initiate_logger(name, log_file_path = None):
 
     logger.setLevel(logging.INFO)
 
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] : %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
     # if you want to save add file handler
     if log_file_path:
-        add_file_handler(logger, log_file_path)
+        add_file_handler(logger, log_file_path, formatter)
 
     # console handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(logging.Formatter("[%(levelname)s] : %(message)s"))
+    console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
     return logger
 
-def add_file_handler(logger, file_path):
+def add_file_handler(logger, file_path, formatter):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     file_handler = logging.FileHandler(file_path, mode="w", encoding="utf-8")
     file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s : %(message)s"))
+    file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
